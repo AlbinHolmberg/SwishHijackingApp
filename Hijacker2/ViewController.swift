fa3e5c5887331568dce1a23682f4ccf2665c76ad
albinholmberg <alho1901@student.miun.se>
2022-04-07 09:45:01 +0200

    Initial Commit

---
+++ Hijacker2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hijacker2
+//
+//  Created by albin holmberg on 2022-04-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
